bfe19a481f8aa8a37446370f4cd907fc9c250d0e
AngryLittleNugget <keenanmwalter@gmail.com>
2025-07-06 21:36:36 -0600

    Sprite + Door
    There is a little dude for the player now, and doors will tell you what key they require.

--- Assets/Scripts/Interactables/LockedDoor.cs
+++ Assets/Scripts/Interactables/LockedDoor.cs
@@ -1,12 +1,33 @@
 using UnityEngine;
-
+using TMPro;
 public class LockedDoor : Interactable
 {
     private GridManager gridManager;
     public string itemNeeded;
+    private Canvas canvas;
+    private TextMeshProUGUI displayText;
+    private bool textIsOn = false;
+
+    void OnEnable()
+    {
+        PlayerMove.OnPlayerMove += TurnOffText;
+    }
+
+    private void OnDisable()
+    {
+        PlayerMove.OnPlayerMove -= TurnOffText;
+    }
     void Start()
     {
         gridManager = FindFirstObjectByType<GridManager>();
+        canvas = GameObject.FindWithTag("UICanvas").GetComponentInChildren<Canvas>();
+        displayText = FindFirstObjectByType<TextMeshProUGUI>();
+
+        if (canvas != null)
+        {
+
+            displayText.text = "";
+        }
     }
 
     // Update is called once per frame
@@ -24,7 +45,22 @@ public class LockedDoor : Interactable
         }
         else
         {
+            displayText.text = $"You need the {this.itemNeeded}";
+              textIsOn = true;
             return false;
         }
     }
+    
+      private void TurnOffText()
+      {
+         // Debug.Log("4: TurnOffText firing");
+          if (textIsOn == true)
+          {
+             // Debug.Log("5: Inner block running.");
+            //canvas.enabled = false;
+            displayText.text = "";
+              textIsOn = false;
+             // Debug.Log($"5.5: canvas.enabled is {canvas.enabled}");
+          }
+      }
 }
